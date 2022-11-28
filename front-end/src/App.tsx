c0e500733955c6548f108b301a337642fe48f7c4
JunHyuk Park <inte99ral@gmail.com>
2022-11-28 16:53:38 +0900

    test1

---
+++ front-end/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { AppRouter } from 'AppRouter';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <AppRouter />
+    </div>
+  );
+}
+
+export default App;
---
+++ front-end/src/AppRouter.tsx
@@ -0,0 +1,23 @@
+// API & Library
+import React from 'react';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+
+// Components
+import { HomePage } from 'components/pages/HomePage';
+import { ErrorPage } from 'components/pages/ErrorPage';
+
+export const AppRouter = () => {
+  // Return
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/home" />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/error" element={<ErrorPage />} />
+          <Route path="/*" element={<Navigate replace to="/error" />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
