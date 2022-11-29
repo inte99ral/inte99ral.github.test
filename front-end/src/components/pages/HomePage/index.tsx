c82bf279e906ba150d254e7b50ca25eee6c608f5
JunHyuk Park <inte99ral@gmail.com>
2022-11-29 15:04:15 +0900

    test11

--- front-end/src/components/pages/HomePage/index.tsx
+++ front-end/src/components/pages/HomePage/index.tsx
@@ -2,10 +2,14 @@
 import React from 'react';
 
 export const HomePage = () => {
+  // Init
+  const version: string = process.env.REACT_APP_VERSION || '0.00v';
+
   // Return
   return (
     <>
       <div>홈</div>
+      <div>{version}</div>
     </>
   );
 };
