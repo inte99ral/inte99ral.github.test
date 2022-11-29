7b684453574321e45d4927c1fb568cb57215c997
JunHyuk Park <inte99ral@gmail.com>
2022-11-29 17:32:22 +0900

    try2.0

--- front-end/src/components/pages/HomePage/index.tsx
+++ front-end/src/components/pages/HomePage/index.tsx
@@ -1,15 +1,18 @@
 // API & Library
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 export const HomePage = () => {
   // Init
   const version: string = process.env.REACT_APP_VERSION || '0.00v';
+  const { p } = useParams();
 
   // Return
   return (
     <>
       <div>홈</div>
-      <div>{version}</div>
+      <div>version: {version}</div>
+      <div>p: {p}</div>
     </>
   );
 };
