14c3ffedd826c2be571ad6e993e06e2d0701cee5
JunHyuk Park <inte99ral@gmail.com>
2022-11-30 10:58:06 +0900

    try2.2 fix

--- front-end/src/components/pages/HomePage/index.tsx
+++ front-end/src/components/pages/HomePage/index.tsx
@@ -1,18 +1,20 @@
 // API & Library
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { useQueryString } from 'api/queryStringLibrary';
 
 export const HomePage = () => {
   // Init
   const version: string = process.env.REACT_APP_VERSION || '0.00v';
-  const { p } = useParams();
+  const location = useLocation();
+  const queryString = useQueryString(location.search);
 
   // Return
   return (
     <>
       <div>홈</div>
       <div>version: {version}</div>
-      <div>p: {p}</div>
+      <div>p: {queryString.get('p')}</div>
     </>
   );
 };
